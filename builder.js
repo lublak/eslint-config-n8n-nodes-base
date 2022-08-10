1733bcd30597c64da2c94e23d9974bb2a72d9744
lublak <lublak.de@gmail.com>
2022-08-10 09:36:42 +0200

    add prettier to builder

--- builder.js
+++ builder.js
@@ -1,4 +1,6 @@
+const prettier = require("prettier");
 const fs = require('fs');
 const { getConfig } = require("./helper");
-
-fs.writeFileSync('index.js', `module.exports = ${JSON.stringify(getConfig(), null, 2)};`);
+const prettierrc = require('./.prettierrc.js');
+prettierrc.parser = 'babel';
+fs.writeFileSync('index.js', prettier.format(`module.exports = ${JSON.stringify(getConfig(), null, 2)};`, prettierrc));
